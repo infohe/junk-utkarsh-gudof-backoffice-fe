345217087682018c3f25d3b92f62e94936fa460e
utkarshsingh99 <utkarshsingh369@gmail.com>
2020-08-11 09:24:53 +0530

    resolved bug for new category save

--- src/axios.js
+++ src/axios.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 
 // axios.defaults.baseURL = 'https://infohebackoffice.herokuapp.com';
 export const instance = axios.create({
-    baseURL: 'https://infohebackoffice.herokuapp.com' 
-    // baseURL : 'http://localhost:4000'
+    // baseURL: node.process.env === 'production' ? 'https://infohebackoffice.herokuapp.com' : 'http://localhost:4000'
+    // baseURL: 'https://infohebackoffice.herokuapp.com' 
+    baseURL : 'http://localhost:4000'
 });
 
 // export default {instance};
