448aeb446128be54089e28993e87e5d6c6627f3a
Nithin PS <47279496+Nithinps021@users.noreply.github.com>
2021-07-23 14:19:27 +0530

    updating the form

--- src/containers/Template/EditTemplate.tsx
+++ src/containers/Template/EditTemplate.tsx
@@ -1,57 +1,39 @@
 import React, { useState, useCallback } from 'react';
-import { useForm } from 'react-hook-form';
-import { v4 as uuidv4 } from 'uuid';
 import { useMutation, gql } from '@apollo/client';
 import { useDrawerDispatch } from 'context/DrawerContext';
 import { Scrollbars } from 'react-custom-scrollbars';
 import { FormBuilder } from '@ginkgo-bioworks/react-json-schema-form-builder';
-import Uploader from 'components/Uploader/Uploader';
-import Input from 'components/Input/Input';
-import Select from 'components/Select/Select';
 import Button, { KIND } from 'components/Button/Button';
 import DrawerBox from 'components/DrawerBox/DrawerBox';
 import { Row, Col } from 'components/FlexBox/FlexBox';
 import {
-  Form,
   DrawerTitleWrapper,
   DrawerTitle,
-  FieldDetails,
   ButtonGroup,
 } from '../DrawerItems/DrawerItems.style';
-import { FormFields, FormLabel } from 'components/FormFields/FormFields';
-import {useTemplateState} from './TemplateContext'
+import { useTemplateState } from './TemplateContext'
 
-const GET_CATEGORIES = gql`
-  query getCategories($type: String, $searchBy: String) {
-    categories(type: $type, searchBy: $searchBy) {
-      id
-      icon
-      name
-      slug
-      type
+const UPDATE_FORMSCHEMA = gql`
+  mutation updateTemplate($template: UpdateTemplateInput!) {
+    updateTemplate(updateTemplateInput: $template) {
+      _id,
+      name,
+      category_id,
+      formSchema
     }
   }
-`;
-const CREATE_CATEGORY = gql`
-  mutation createCategory($category: AddCategoryInput!) {
-    createCategory(category: $category) {
-      id
-      name
-      type
-      icon
-      # creation_date
-      slug
-      # number_of_product
+`
+const GET_TEMPLATE = gql`
+    query   {
+        allTemplate{
+            _id,
+            name,
+            category_id,
+            formSchema
+        }
     }
-  }
 `;
 
-const options = [
-  { value: 'grocery', name: 'Grocery', id: '1' },
-  { value: 'women-cloths', name: 'Women Cloths', id: '2' },
-  { value: 'bags', name: 'Bags', id: '3' },
-  { value: 'makeup', name: 'Makeup', id: '4' },
-];
 type Props = any;
 
 const AddCategory: React.FC<Props> = (props) => {
@@ -59,90 +41,98 @@ const AddCategory: React.FC<Props> = (props) => {
   const closeDrawer = useCallback(() => dispatch({ type: 'CLOSE_DRAWER' }), [
     dispatch,
   ]);
-  const formSchema=JSON.parse(useTemplateState('formSchema'))
-  console.log(formSchema)
-  const { register, handleSubmit, setValue } = useForm();
-  const [schema, setSchema] = React.useState(formSchema);
-  const [uischema, setUiSchema] = React.useState('{}');
-  const [category, setCategory] = useState([]);
-  React.useEffect(() => {
-    register({ name: 'parent' });
-    register({ name: 'image' });
-  }, [register]);
-  const [createCategory] = useMutation(CREATE_CATEGORY, {
-    update(cache, { data: { createCategory } }) {
-      const { categories } = cache.readQuery({
-        query: GET_CATEGORIES,
-      });
-
-      cache.writeQuery({
-        query: GET_CATEGORIES,
-        data: { categories: categories.concat([createCategory]) },
-      });
+  const formSchema = JSON.parse(useTemplateState('formSchema'))
+  const templateId = useTemplateState('_id')
+  const [schema, setSchema] = useState(formSchema);
+  const [uischema, setUiSchema] = useState('{}');
+  const [updateTemplate] = useMutation(UPDATE_FORMSCHEMA,{
+    onCompleted(data){
     },
-  });
+    onError(error){
+    }
+  })
+  const onSubmit = () => {
+    console.log('submitted')
+    updateTemplate({
+      variables: { template: { _id:templateId, formSchema: JSON.stringify(schema) } },
+      refetchQueries: [{ query: GET_TEMPLATE }]
+    })
+    closeDrawer()
+  };
 
-  const onSubmit = ({ name, slug, parent, image }) => {
-    const newCategory = {
-      id: uuidv4(),
-      name: name,
-      type: parent[0].value,
-      slug: slug,
-      icon: image,
-      creation_date: new Date(),
-    };
-    createCategory({
-      variables: { category: newCategory },
-    });
-    closeDrawer();
-    console.log(newCategory, 'newCategory');
-  };
-  const handleChange = ({ value }) => {
-    setValue('parent', value);
-    setCategory(value);
-  };
-  const handleUploader = (files) => {
-    setValue('image', files[0].path);
-  };
- 
+
   return (
     <>
       <DrawerTitleWrapper>
         <DrawerTitle>Edit Template</DrawerTitle>
       </DrawerTitleWrapper>
 
-      <Form onSubmit={handleSubmit(onSubmit)} style={{ height: '100%' }}>
-        <Scrollbars
-          autoHide
-          renderView={(props) => (
-            <div {...props} style={{ ...props.style, overflowX: 'hidden' }} />
-          )}
-          renderTrackHorizontal={(props) => (
-            <div
-              {...props}
-              style={{ display: 'none' }}
-              className="track-horizontal"
-            />
-          )}
+      <Scrollbars
+        autoHide
+        renderView={(props) => (
+          <div {...props} style={{ ...props.style, overflowX: 'hidden' }} />
+        )}
+        renderTrackHorizontal={(props) => (
+          <div
+            {...props}
+            style={{ display: 'none' }}
+            className="track-horizontal"
+          />
+        )}
+      >
+        <Row>
+          <Col xs={12}>
+            <DrawerBox>
+              <FormBuilder
+                schema={schema}
+                uischema={uischema}
+                onChange={(newSchema: string, newUiSchema: string) => {
+                  setSchema(newSchema);
+                  setUiSchema(newUiSchema)
+                }}
+              />
+            </DrawerBox>
+          </Col>
+        </Row>
+      </Scrollbars>
+      <ButtonGroup>
+        <Button
+          kind={KIND.minimal}
+          onClick={closeDrawer}
+          overrides={{
+            BaseButton: {
+              style: ({ $theme }) => ({
+                width: '50%',
+                borderTopLeftRadius: '3px',
+                borderTopRightRadius: '3px',
+                borderBottomRightRadius: '3px',
+                borderBottomLeftRadius: '3px',
+                marginRight: '15px',
+                color: $theme.colors.red400,
+              }),
+            },
+          }}
         >
-           <Row>
-            <Col xs={12}>
-              <DrawerBox>
-                <FormBuilder
-                  schema={schema}
-                  uischema={uischema}
-                  onChange={(newSchema: string, newUiSchema: string) => {
-                    setSchema(newSchema);
-                    setUiSchema(newUiSchema)
-                  }}
-                />
-              </DrawerBox>
-            </Col>
-          </Row>
-         
+          Cancel
+        </Button>
 
-        </Scrollbars>
-      </Form>
+        <Button
+          onClick={onSubmit}
+          overrides={{
+            BaseButton: {
+              style: ({ $theme }) => ({
+                width: '50%',
+                borderTopLeftRadius: '3px',
+                borderTopRightRadius: '3px',
+                borderBottomRightRadius: '3px',
+                borderBottomLeftRadius: '3px',
+              }),
+            },
+          }}
+        >
+          update template
+        </Button>
+      </ButtonGroup>
     </>
   );
 };
